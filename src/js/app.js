156fa270c613b4090b7393e26f031b0e1b8273c7
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-07-15 23:39:01 +0300

    game files upload

---
+++ src/js/app.js
@@ -0,0 +1,122 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-eval */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-alert */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable function-paren-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable no-plusplus */
+/* eslint-disable lines-around-directive */
+/* eslint-disable strict */
+'use strict';
+
+const cardItemsRow = document.getElementsByClassName('card_items-row')[0];
+const cardFlipItem = document.getElementsByClassName('card_flip-item');
+
+window.onload = () => {
+  const flipAnFlip = (method) => {
+    [...cardFlipItem].forEach((card) => {
+      eval(`card.classList.${method}('flip')`);
+    });
+  };
+
+  flipAnFlip('add');
+
+  setTimeout(() => {
+    flipAnFlip('remove');
+  }, 1500);
+};
+
+const randomSortArr = (array) => array.sort(() => Math.random() - 0.5);
+
+function isWin() {
+  alert('Игра пройдена!');
+  window.location.reload();
+}
+
+const lastTargetId = [];
+const lastTargetDataName = [];
+
+function flipper(e) {
+  const { target } = e;
+  const targetParentCart = target.closest('.card_flip-item');
+
+  targetParentCart.classList.add('flip');
+
+  lastTargetId.push(targetParentCart.dataset.id);
+  lastTargetDataName.push(targetParentCart.dataset.name);
+
+  const flips = cardItemsRow.getElementsByClassName('flip');
+
+  const targetDataName = targetParentCart.dataset.name;
+  const prevTargetDataName = lastTargetDataName[lastTargetDataName.length - 2];
+  const targetDataId = targetParentCart.dataset.id;
+  const prevTargetDataId = lastTargetId[lastTargetId.length - 2];
+
+  if (targetDataName !== prevTargetDataName && flips.length === 2) {
+    setTimeout(() => {
+      [...flips].forEach((flippers) => flippers.classList.remove('flip'));
+    }, 500);
+  }
+  if (targetDataName === prevTargetDataName && targetDataId !== prevTargetDataId) {
+    [...flips].forEach((flippers) => {
+      flippers.classList.remove('flip');
+      flippers.classList.add('flip_active');
+
+      const allCardActive = [...cardItemsRow.children].every((card) =>
+        card.classList.contains('flip_active')
+      );
+
+      if (allCardActive) {
+        setTimeout(() => isWin(), 1000);
+      }
+    });
+  }
+}
+
+cardItemsRow.addEventListener('click', flipper);
+
+function addCardsToRow() {
+  const cardsList = [];
+  const cardDataName = ['burger', 'pancakes', 'toast', 'ramen', 'pizza'];
+
+  let count = 1;
+  for (let i = 0; i < 2; i++) {
+    cardDataName.forEach((dataName) => {
+      const cardItem = `<div class="card_flip-item" data-id="${count++}" data-name=${dataName}>
+            <div class="card_front">?</div>
+            <div class="card_back"></div>
+          </div>`;
+
+      cardsList.push(cardItem);
+    });
+  }
+
+  randomSortArr(cardsList).forEach((card) => cardItemsRow.insertAdjacentHTML('beforeend', card));
+}
+addCardsToRow();
+
+function addBackgroundToCards() {
+  [...cardFlipItem].forEach((card) => {
+    const getElemAndAddClass = (className) =>
+      card.getElementsByClassName('card_back')[0].classList.add(className);
+
+    if (card.dataset.name === 'burger') {
+      getElemAndAddClass('bg-card-burger');
+    }
+    if (card.dataset.name === 'pancakes') {
+      getElemAndAddClass('bg-card-pancakes');
+    }
+    if (card.dataset.name === 'toast') {
+      getElemAndAddClass('bg-card-toast');
+    }
+    if (card.dataset.name === 'ramen') {
+      getElemAndAddClass('bg-card-ramen');
+    }
+    if (card.dataset.name === 'pizza') {
+      getElemAndAddClass('bg-card-pizza');
+    }
+  });
+}
+addBackgroundToCards();
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff2?|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          {
+            loader: 'xml-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
